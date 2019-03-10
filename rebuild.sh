a2dda84ab85beb57ff5d3eb469444ff3b913c3b1
Indospace.io <justin@indospace.io>
2019-03-10 10:45:38 -0700

    remove unnecessary lines of code
    This is not necessary...right?
    
    set -ex
    TZ="2018i"

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-set -ex
-TZ="2018i"
-
+#!/bin/bas
 rm -rf timezones.geojson.zip dist ne_10m_urban_areas.*
 curl -L --retry 3 -C - \
   -O "https://github.com/evansiroky/timezone-boundary-builder/releases/download/$TZ/timezones.geojson.zip" \
